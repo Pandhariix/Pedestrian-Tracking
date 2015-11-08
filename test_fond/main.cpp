05f29cfb21268beb5ed7465c98c31c6bf4ed476d
Pandhariix <b11max@yahoo.fr>
2015-11-08 20:20:59 +0100

    modification de code pour detecter arrière plan et ajout de data video

--- test_fond/main.cpp
+++ test_fond/main.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 int main( int argc, char** argv )
 {
+
+    /// INITIAL TEST PROGRAM
+    /*
     if( argc != 2)
     {
      std::cout <<" Usage: display_image ImageToLoadAndDisplay" << std::endl;
@@ -52,4 +55,44 @@ int main( int argc, char** argv )
 
     cv::waitKey(0);                                          // Wait for a keystroke in the window
     return 0;
+    */
+
+    ///OFFICIAL TEST FOND PROGRAM
+    if(argc != 3)
+    {
+        std::cout<<"unable to launch the program, please enter correct input arguments"<<std::endl;
+    }
+
+    //init the matrix
+    cv::Mat imageFond;
+    cv::Mat imageCompare;
+    cv::Mat imageFondGray;
+    cv::Mat imageCompareGray;
+    cv::Mat imageDiff;
+
+    imageFond = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    imageCompare = cv::imread(argv[2], CV_LOAD_IMAGE_COLOR);
+
+    if(!imageFond.data || !imageCompare.data)
+    {
+        std::cout<<"at least one of the images can not be read"<<std::endl;
+        return -1;
+    }
+
+    //convert both images to grayscale
+
+    cv::cvtColor(imageFond, imageFondGray, CV_BGR2GRAY);
+    cv::cvtColor(imageCompare, imageCompareGray, CV_BGR2GRAY);
+
+    //difference between the two images
+    cv::absdiff(imageFondGray, imageCompareGray, imageDiff);
+
+    //display
+    cv::namedWindow( "test_fond", cv::WINDOW_AUTOSIZE );// Create a window for display.
+    cv::imshow( "test_fond", imageDiff);
+
+    cv::waitKey(0);
+    return(0);
+
+
 }
